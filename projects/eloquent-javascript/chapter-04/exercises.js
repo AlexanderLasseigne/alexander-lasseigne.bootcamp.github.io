cfe4368223cc32d9fb113d9a859b6dc8de432a50
Alexander Lasseigne III <alexanderlasseigne3rd@gmail.com>
2022-09-01 01:35:37 +0000

    ahgh so much

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -47,8 +47,13 @@ function reverseArray(arr) {
 // reverseArrayInPlace /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArrayInPlace() {
-
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++){
+    let tmp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tmp;
+  }
+  return arr;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
