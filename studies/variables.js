b70c25300b2d95a00079c77b1a75ca6509a2771e
Alexander Lasseigne III <alexanderlasseigne3rd@gmail.com>
2022-09-04 23:03:58 +0000

    in studies

--- studies/variables.js
+++ studies/variables.js
@@ -6,20 +6,21 @@
  * particular type, like a Number, String,
  * Boolean, Array, Object or another data-type. Variables
  * are called so because once created, we
- * can CHANGE the value (and type of value) to which they
+ * can often CHANGE the value (and type of value) to which they
  * point.
  * 
- * 1.  There are 2 phases of using variables: declaration
+ * 1. There are 2 phases of using variables: declaration
  * and initialization (or assignment).
  * 
- * 2.  To create a variable we use the keyword, var, 
- * followed by a name (id or alias) for our
- * variable.
+ * 2. The keywords var, let, const.
  * 
- * 3.  Hoisting
+ * 3. Hoisting
+ * 
  * 
 // 
-/* 1. declaration //
+
+// 1. declaration and initialization (or assignment/reassignment).//
+/* 
 At the declaration phase, the variable
 myName is undefined b/c we have NOT
 initialized it to anything
@@ -27,12 +28,20 @@ initialized it to anything
 var myName;
 console.log(myName); // prints => undefined
 
-// 2. initialization or assignment
-// When you assign a value to an undeclared variable.
+// 1.a. initialization or assignment //
+// When you assign a value to an undeclared variable. To create a variable 
+// in this example, we use the keyword, var, 
+// followed by a name (id or alias) for our variable.
 myName = 'john';
 console.log(myName); // prints => john
 
-// 3. Re-assignment
-//Changing or updating teh value of a previously assigned variable
+// 1.b. Re-assignment //
+//Changing or updating the value of a previously assigned variable
 myName = 'bob';
 console.log(myName); // prints => bob
+
+
+// 2. The keywords var, let, const. //
+/*
+
+*/
